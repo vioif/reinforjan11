62d8599ecf10921c6a0ac2c0655981bb3f257a7a
vioif <ireneaflores3@gmail.com>
2018-01-11 14:48:20 -0500

    fixed code

---
+++ exercise.rb
@@ -0,0 +1,21 @@
+def select_cards(possible_cards, hand)
+
+  possible_cards.each do |current_card|
+    print "Do you want to pick up #{current_card}?"
+    answer = gets.chomp
+    if answer.downcase == 'y'
+      hand << current_card
+    if hand.count <= 3
+    end
+    return hand
+    end
+  end
+end
+
+available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
+
+new_hand = select_cards(available_cards, [])
+
+display_hand = new_hand.join("\n")
+
+puts "Your new hand is: \n#{display_hand}"
